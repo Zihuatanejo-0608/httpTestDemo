62fdf9c7f2957434bd76ac0d55d978d0f82b9007
chukaimin-it@bestpay.com.cn <chukaimin-it@bestpay.com.cn>
2020-04-09 09:57:04 +0800

    修改hutools测试框架,新增判断

--- httpTestCode/src/main/java/toolsUnit/HuToolHttpUtil.java
+++ httpTestCode/src/main/java/toolsUnit/HuToolHttpUtil.java
@@ -1,5 +1,6 @@
 package toolsUnit;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
@@ -13,7 +14,10 @@ import cn.hutool.json.JSONUtil;
 public class HuToolHttpUtil {
 
     public static JSONObject post(String url, String headerName, String token, JSONObject parameter) throws Exception{
-        HttpRequest httpRequest = HttpUtil.createPost(url).header(headerName, token).timeout(5000);
+        HttpRequest httpRequest = HttpUtil.createPost(url).timeout(5000);
+        if (StrUtil.isNotBlank(headerName) && StrUtil.isNotBlank(token)){
+            httpRequest.header(headerName,token);
+        }
         if (parameter != null){
             httpRequest.body(JSONUtil.toJsonStr(parameter));
             System.out.println("请求对象:" + parameter);
@@ -28,7 +32,11 @@ public class HuToolHttpUtil {
     }
 
     public static JSONObject get(String url, String headerName, String token) throws Exception{
-        HttpResponse response = HttpUtil.createGet(url).header(headerName, token).timeout(5000).execute();
+        HttpRequest httpRequest = HttpUtil.createGet(url).timeout(5000);
+        if (StrUtil.isNotBlank(headerName) && StrUtil.isNotBlank(token)){
+            httpRequest.header(headerName,token);
+        }
+        HttpResponse response = httpRequest.execute();
         if (response.isOk()){
             System.out.println("请求结果:" + response.body());
             return new JSONObject(response.body());
@@ -38,7 +46,11 @@ public class HuToolHttpUtil {
     }
 
     public static JSONObject delete(String url, String headerName, String token) throws Exception{
-        HttpResponse response = HttpUtil.createRequest(Method.DELETE,url).header(headerName, token).timeout(5000).execute();
+        HttpRequest httpRequest = HttpUtil.createRequest(Method.DELETE, url).timeout(5000);
+        if (StrUtil.isNotBlank(headerName) && StrUtil.isNotBlank(token)){
+            httpRequest.header(headerName,token);
+        }
+        HttpResponse response = httpRequest.execute();
         if (response.isOk()){
             System.out.println("请求结果:" + response.body());
             return new JSONObject(response.body());
@@ -48,7 +60,11 @@ public class HuToolHttpUtil {
     }
 
     public static JSONObject put(String url, String headerName, String token, JSONObject parameter) throws Exception{
-        HttpResponse response = HttpUtil.createRequest(Method.PUT,url).header(headerName, token).body(JSONUtil.toJsonStr(parameter)).timeout(5000).execute();
+        HttpRequest httpRequest = HttpUtil.createRequest(Method.PUT, url).body(JSONUtil.toJsonStr(parameter)).timeout(5000);
+        if (StrUtil.isNotBlank(headerName) && StrUtil.isNotBlank(token)){
+            httpRequest.header(headerName,token);
+        }
+        HttpResponse response = httpRequest.execute();
         System.out.println("请求对象:" + parameter);
         if (response.isOk()){
             System.out.println("请求结果:" + response.body());
